996cf2200affda851896dbe93c64b6524db4ad68
Acer <it21173318@my.sliit.lk>
2023-05-15 08:20:51 +0530

    commit 4

--- app/src/main/java/com/example/billwise/ElectricBill.kt
+++ app/src/main/java/com/example/billwise/ElectricBill.kt
@@ -2,10 +2,60 @@ package com.example.billwise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+import com.example.billwise.databinding.ActivityElectricBillBinding
+import java.nio.file.Files.delete
+
 
 class ElectricBill : AppCompatActivity() {
+
+
+    private lateinit var binding:ActivityElectricBillBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_electric_bill)
+
+        initialiseBinding()
+
+        binding.electricDisplay.setOnClickListener{
+            calculateElectricBill()
+        }
+        binding.elecDelete.setOnClickListener {
+            delete()
+        }
     }
+    private fun initialiseBinding(){
+        binding = ActivityElectricBillBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun calculateElectricBill( ){
+
+        val enddate = binding.Enddate.text.toString()
+        val startdate = binding.StartDate.text.toString()
+        val unitPrice =binding.unitP.text.toString()
+        val units = binding.usedUnits.text.toString()
+
+        val end = enddate.toInt()
+        val start = startdate.toInt()
+        val unit = unitPrice.toFloat()
+        val useduni = units.toFloat()
+
+        val result = (((end-start)*unit)*useduni)
+
+
+        val displayString = result.toString()
+        binding.resultText.text =displayString
+
+    }
+
+    private fun delete(){
+        binding.resultText.text =null
+        binding.Enddate.text?.clear()
+        binding.StartDate.text?.clear()
+        binding.unitP.text?.clear()
+        binding.usedUnits.text?.clear()
+    }
+
 }
