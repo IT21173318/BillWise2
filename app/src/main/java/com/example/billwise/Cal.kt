7ebd306c311d95126610146cf7461341f62c1d71
Acer <it21173318@my.sliit.lk>
2023-05-15 10:18:15 +0530

    commit 9

--- app/src/main/java/com/example/billwise/Cal.kt
+++ app/src/main/java/com/example/billwise/Cal.kt
@@ -11,7 +11,7 @@ import android.widget.EditText
 import com.example.billwise.models.Calculator
 
 class Cal : Fragment() {
-
+//Declare variables
     private lateinit var edtNumber1: EditText
     private lateinit var edtNumber2: EditText
 
@@ -22,14 +22,17 @@ class Cal : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_cal, container, false)
 
+        //get numbers inputs
         edtNumber1 = view.findViewById(R.id.et_num1)
         edtNumber2 = view.findViewById(R.id.et_num2)
 
+        // get assign details into button
         val buttonPlus: Button = view.findViewById(R.id.btn_plus)
         val buttonMinus: Button = view.findViewById(R.id.btn_minus)
         val buttonMultiply: Button = view.findViewById(R.id.btn_multiply)
         val buttonDivide: Button = view.findViewById(R.id.btn_divide)
 
+        //create functions
         buttonPlus.setOnClickListener {
             calculateResult(it, "+")
         }
@@ -49,11 +52,13 @@ class Cal : Fragment() {
         return view
     }
 
+    //calculate result
     private fun calculateResult(view: View, operator: String) {
         val calculator = Calculator(
             edtNumber1.text.toString().toDouble(),
             edtNumber2.text.toString().toDouble()
         )
+        //operations
         val ans = when(operator) {
             "+" -> calculator.add()
             "-" -> calculator.subtract()
@@ -61,8 +66,9 @@ class Cal : Fragment() {
             "/" -> calculator.divide()
             else -> 0.0
         }
+        //print answer
         println(ans)
-
+        //Riderect into ResultActivity
         val intent = Intent(activity, ResultActivity::class.java)
         intent.putExtra("answer", ans)
         startActivity(intent)
